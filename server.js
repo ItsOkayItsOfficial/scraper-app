215ba9df5987430c5c4583ba01a31b00c1e07a7c
Alex P <alexp@itsokayitsofficial.io>
2017-11-19 23:06:15 -0700

    Basic dependencies set

--- server.js
+++ server.js
@@ -8,3 +8,41 @@
 * URL: github.com/itsokayitsofficial/scraper-app
 */
 
+// VARIABLES - Dependencies
+var path = require('path');
+var request = require('request');
+var cheerio = require('cheerio');
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var ObjectId = require('mongojs').ObjectID;
+var ScrapedData = require('./scrapedDataModel');
+
+// INIT - Handlebars engine
+var hbs = exphbs.create({
+  defaultLayout: 'main',
+  // Specify helpers which are only registered on this instance.
+  helpers: {
+    addOne: function(value, options){
+      return parseInt(value) + 1;
+    }
+  }
+});
+
+// INIT - Express app
+var app = express();
+
+// EXPRESS - Handlebars init
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// DATABASE - Mongoose db connection
+mongoose.connect('mongodb://localhost/scraper');
+var db = mongoose.connection;
+
+
+// LISTEN - Express app PORT
+app.listen(3000, function() {
+  console.log('App running on port 3000!');
+});
